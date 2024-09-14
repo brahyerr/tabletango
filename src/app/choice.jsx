3042f0a0c1c668376d75d0f4b795f499afdfe7a7
brahyerr <head@phams.page>
2024-09-14 18:18:33 -0400

    Commit #3

--- src/app/choice.jsx
+++ src/app/choice.jsx
@@ -1,28 +1,15 @@
 import useState from 'react';
-import { GetPrompts } from './input.jsx';
 import styles from './page.module.css'
-import { LikeButton, DislikeButton } from './button.jsx';
-export default function Card(props) {
+export default function Choices() {
     // const [input, setInput] = useState([])
-    const prompts = GetPrompts();
     return (
         <>
         <div style={{width: '100%', height: '100%', paddingTop: 111, paddingBottom: 229, paddingLeft: 116, paddingRight: 116, background: 'linear-gradient(0deg, #90DBBC 0%, #90DBBC 100%), linear-gradient(160deg, rgba(0, 255, 255, 0) 0%, rgba(113.69, 187.17, 255, 0.18) 35%, rgba(0, 25.50, 255, 0.50) 100%)', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', display: 'inline-flex'}}>
-    <div style={{width: 502, height: 737, position: 'relative', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'flex'}}>
-        <div style={{width: 502, height: 737, position: 'relative'}}>
+        <div style={{width: 502, height: 737, position: 'relative', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'flex'}}>
+          <div style={{width: 502, height: 737, position: 'relative'}}>
             <div style={{width: 502, height: 737, left: 0, top: 0, position: 'absolute', background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 53}} />
             <div style={{width: 388, height: 102, left: 56, top: 46, position: 'absolute', color: 'black', fontSize: 35, fontFamily: 'Instrument Sans', fontWeight: '400', wordWrap: 'break-word'}}>Pick your top choice</div>
-        </div>
-         <div style={{width: 159, height: 88, position: 'relative'}}>
-            
-            <div style={{width: 101, height: 37, left: 29, top: 13, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 25, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Middle Eastern</div>
-        </div>
-        </div>
-
-</div>
-        <div>
-          <h1 className={styles.title}>{prompts[0]}</h1>
-          THIS IS A CARD
+          </div>
         </div>
         {/* image */}
         <div style={{width: 502, height: 737, position: 'relative'}}>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,11 +11,29 @@ export default function Home() {
   }
 
   // the main page
+
   
   return (
+
     <><div className={styles.page}>
       <main className={styles.card}>
-        <h1 className={styles.title}>Question Here</h1>
+
+        {/* category options (hardcoded) */}
+        {/*
+        <div style={{width: '100%', height: '100%', paddingTop: 111, paddingBottom: 229, paddingLeft: 116, paddingRight: 116, background: 'linear-gradient(0deg, #90DBBC 0%, #90DBBC 100%), linear-gradient(160deg, rgba(0, 255, 255, 0) 0%, rgba(113.69, 187.17, 255, 0.18) 35%, rgba(0, 25.50, 255, 0.50) 100%)', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', display: 'inline-flex'}}>
+    <div style={{width: 502, height: 737, position: 'relative', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'flex'}}>
+        <div style={{width: 502, height: 737, position: 'relative'}}>
+            <div style={{width: 502, height: 737, left: 0, top: 0, position: 'absolute', background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 53}} />
+            <div style={{width: 388, height: 102, left: 56, top: 46, position: 'absolute', color: 'black', fontSize: 35, fontFamily: 'Instrument Sans', fontWeight: '400', wordWrap: 'break-word'}}>Pick your top choice</div>
+        </div>
+         <div style={{width: 159, height: 88, position: 'relative'}}>
+            
+            <div style={{width: 101, height: 37, left: 29, top: 13, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 25, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Middle Eastern</div>
+        </div>
+        </div>
+
+</div>
+      */}
         <Card />
       </main>
       <footer className={styles.footer}>
